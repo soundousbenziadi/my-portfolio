2057780f202c75860db28a09102b22352914a2e7
soundous <bsoundous112@gmail.com>
2025-01-14 17:53:17 +0100

    Add responsive navbar with HTML, CSS, and JavaScrip

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const switchBtn=document.querySelector("#theme-switch");
 let body=document.querySelector("body");
 let currentTheme=localStorage.getItem("theme");
+const media=window.matchMedia("(width<700px)");
 function enableLightMode(){
    body.classList.remove("dark-mode");
    localStorage.setItem("theme","light");
@@ -16,3 +17,33 @@ switchBtn.addEventListener("click",function(){
 if(currentTheme==="dark"){
    disableLightMode();
 }
+const navBar=document.getElementById("navbar");
+let closeSideBarBtn=document.getElementById("close-sidebar-button");
+let openSideBarBtn=document.getElementById("open-sidebar-button");
+const overlay=document.getElementById("overlay");
+function openSideBar(){
+    navBar.classList.add("show");
+    overlay.style.display="block";
+    openSideBarBtn.style.display="none";
+    navBar.removeAttribute("inert");
+}
+function closeSideBar(){
+  navBar.classList.remove("show");
+  overlay.style.display="none";
+  openSideBarBtn.style.display="block";
+  navBar.setAttribute("inert", "");
+}
+openSideBarBtn.addEventListener("click",openSideBar);
+closeSideBarBtn.addEventListener("click",closeSideBar);
+overlay.addEventListener("click",closeSideBar);
+//  make elements of navBar non-interactive when not used
+function updateNavBar(e){
+   let isMobile=e.matches;
+   if(isMobile){
+      navBar.setAttribute('inert','');
+   }else{
+      navBar.removeAttribute('inert');
+   }
+   }
+   updateNavBar(media);
+   media.addEventListener("change",(e)=>updateNavBar(e));
